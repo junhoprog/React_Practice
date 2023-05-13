e515514718225c95375e0b943c34907e459f91cb
김준호 <kimjunho1231@naver.com>
2023-05-13 14:11:47 +0900

    Study React

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,59 +1,69 @@
-/*eslint-disable*/
-
 import "./App.css";
 import { useState } from "react";
 
 function App() {
   let title = "ReactBlog";
-  //인덱싱
   let [글제목, set글제목] = useState([
     "남자코트추천",
     "강남우동맛집",
     "파이썬독학",
   ]);
-let[modal,setModal]=useState(false);//ui의 상태 넣어놓음->특정 상황에 setModal로 변경
+
+let[따봉,set따봉]=useState([0,0,0]);
+let[Title,setTitle]=useState(0);
+let [입력값,입력값변경]=useState('');
+let[modal,setModal]=useState(false);
+ [1,2,3].map(function(a){
+  return '123314'
+ })
+ 
   return (
     <div className="App">
       <div className="black-nav">
         <h4 style={{ color: "white", fontSize: "16px" }}>{title}</h4>
       </div>
 
-      <div className="list">
-        <h4 onClick={()=>{setModal(true)}}>
-          {글제목[0]}
-        </h4>
-          <p>2월 17일 발행</p>
-      </div>
+      {
+        글제목.map(function(a,i){
+          return (
+            <div className="list">
+            <h4 onClick={()=>{setModal(!modal); setTitle(i)}}>
+              {a}
+            </h4>
+              <p>2월 17일 발행</p>
+              <button onClick={()=>{
+        let copy=[...글제목];
+        copy.splice(i,1);
+        set글제목(copy)
+       }}>글 삭제</button>
+          </div>
+          )
+        })
+      }
+      
 
-      <div className="list">
-        <h4>{글제목[1]}</h4>
-        <p>2월 17일 발행</p>
-      </div>
+<input onChange={(e)=>{입력값변경(e.target.value)}}></input>
 
-      <div className="list">
-        <h4>{글제목[2]}</h4>
-        <p>2월 17일 발행</p>
-      </div>
-
-      <button onClick={()=>{
-        let copy=[...글제목]; 
-        copy[0]='여자코트추천';
+<button onClick={()=>{
+        let copy=[...글제목];
+        copy.unshift(입력값);
         set글제목(copy);
-        }}>글수정</button>
+      }}>글 추가</button>
 
        { 
-        modal ? <Modal></Modal> :<div></div>
+        modal ? <Modal i={0} Title={Title} 글제목={글제목}></Modal> :null
        }
     </div>
   );
-}
+      }
 
-function Modal(){
+function Modal(props){
 return(
   <div className="modal">
-  <h4>제목</h4>
+  <h4>{props.글제목[props.Title]}</h4>
   <p>날짜</p>
   <p>상세 내용</p>
+  <button>글수정</button>
   </div>
   )
 }
